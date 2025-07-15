ff88586a9f2de7ad0e739112348f301af2641009
Jan <107766070+Jan-Stangelj@users.noreply.github.com>
2025-07-15 21:29:31 +0200

    Naloga 4

--- naloga4.cpp
+++ naloga4.cpp
@@ -1,6 +1,40 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+
+struct paket {
+    int prostornina;
+    int placilo;
+    float placiloNaEnoto;
+    unsigned long ogMesto;
+};
+
+bool primerjaj(const paket& a, const paket& b) {
+    return a.placiloNaEnoto > b.placiloNaEnoto;
+}
 
 int main() {
-    std::cout << "Hello world!\n";
+    int stPaketov = 0;
+    std::cin >> stPaketov;
+
+    std::vector<paket> paketi;
+    paketi.reserve(stPaketov);
+
+    for (int i = 0; i < stPaketov; i++) {
+        int prostornina, placilo;
+        std::cin >> prostornina >> placilo;
+        paket p = {prostornina, placilo, (float)placilo/(float)prostornina, paketi.size()};
+        paketi.push_back(p);
+    }
+
+    std::sort(paketi.begin(), paketi.end(), primerjaj);
+
+    int preostalProstor = 100;
+    for (paket p : paketi) {
+        preostalProstor -= p.prostornina;
+        if (preostalProstor < 0) 
+            break;
+        std::cout << p.ogMesto << ' ';
+    }
     return 0;
 }
