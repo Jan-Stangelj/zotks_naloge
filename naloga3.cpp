5cdbc662ddd0052381377993cafa67ff52afece1
Jan <107766070+Jan-Stangelj@users.noreply.github.com>
2025-07-15 20:54:09 +0200

    Naloga 3

--- naloga3.cpp
+++ naloga3.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+#include <string>
+
+std::string besedaNazaj(const std::string& beseda) {
+    std::string rezultat;
+    for (int i = beseda.length() - 1; i != -1; i--) {
+        rezultat.push_back(beseda[i]);
+    }
+    return rezultat;
+}
 
 int main() {
-    std::cout << "Hello world!\n";
+    std::string beseda;
+    std::cin >> beseda;
+
+    int odstranjeneCrke = 0;
+
+    if (beseda == besedaNazaj(beseda)) {
+        std::cout << odstranjeneCrke << '\n';
+        return 0;
+    }
+
+    while (true) {
+        odstranjeneCrke++;
+        beseda.pop_back();
+        if (beseda == besedaNazaj(beseda)) {
+            std::cout << odstranjeneCrke << '\n';
+            break;
+        }
+    }
+
     return 0;
 }
